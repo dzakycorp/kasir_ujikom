e760c489bc441ade0e0ce92a505dc0dc4869b476
dzakycorp <muhammaddzaky4019@gmail.com>
2023-11-28 08:40:10 +0700

    baru

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,85 @@
+@if(session("Error"))
+   <div class= "alert alert-danger">{{session("Error")}}</div>
+   @endif
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form Login</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #033E3E;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        
+        .login-container {
+            background-color: #ffff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 400px;
+            padding: 20px;
+        }
+        
+        .login-container h2 {
+            text-align: center;
+            color: #333;
+        }
+        
+        .form-group {
+            margin: 8px;
+            
+        }
+        
+        .form-group label {
+            display: block;
+            font-weight: bold;
+        }
+        
+        .form-group input {
+            width: 370px;
+            height: 25px;
+            padding: 6px;
+            border: 1px solid #ccc;
+            border-radius: 7px;
+        }
+        
+        .form-group button {
+            width: 100%;
+            background-color: #033E3E;
+            color: #fff;
+            border: none;
+            padding: 10px;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+        
+        .form-group button:hover {
+            background-color: #033E3E;
+        }
+    </style>
 </head>
 <body>
-    <h1>loginnn</h1>
+    <div class="login-container">
+        <h2>LOGIN</h2>
+        <form action="{{url('/login')}}" method="post">
+          @method('post')
+          @csrf
+            <div class="form-group">
+                <label for="username">username</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <button type="submit">Masuk</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,11 +1,94 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form Pendaftaran</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #033E3E;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .register-container {
+            background-color: #fff;
+            border-radius: 7px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 400px;
+            padding: 20px;
+        }
+
+        .register-container h2 {
+            text-align: center;
+            color: #333;
+        }
+
+        .form-group {
+            margin: 10px 0;
+        }
+
+        .form-group label {
+            display: block;
+            font-weight: bold;
+        }
+
+        .form-group input {
+            width: 375px;
+            height: 20px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 7px;
+        }
+
+        .form-group button {
+            width: 100%;
+            background-color: #033E3E;
+            color: #fff;
+            border: none;
+            padding: 10px;
+            border-radius: 7px;
+            cursor: pointer;
+        }
+
+        .form-group button:hover {
+            background-color: #033E3E;
+        }
+    </style>
 </head>
 <body>
-    <h1>register</h1>
+    <div class="register-container">
+        <h2>DAFTAR</h2>
+        <form action="/register" method="post">
+        @method('post')
+        @csrf
+
+        <div class="form-group">
+                <label for="nama">Nik</label>
+                <input type="text" id="nik" name="nik" required>
+            </div>
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" id="nama" name="nama" required>
+            </div>
+            <div class="form-group">
+                <label for="email">username</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <label for="telepon">nomor telepon</label>
+                <input type="tel" id="telp" name="telp" required>
+            </div>
+            <div class="form-group">
+                <button type="submit">Daftar</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
